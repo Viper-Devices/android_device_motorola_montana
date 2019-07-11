0cb4b82bd57ec03e278ec76c66ef90222938a679
EsromTech <esromgalaxy5@gmail.com>
2019-07-11 14:53:15 -0300

    BringUp ViperOS

--- viper_montana.mk
+++ viper_montana.mk
@@ -22,8 +22,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 $(call inherit-product, device/motorola/montana/device.mk)
 $(call inherit-product, vendor/motorola/montana/montana-vendor.mk)
 
-# Inherit some common Kraken stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common ViperOS stuff.
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
@@ -35,7 +35,7 @@ TARGET_SCREEN_HEIGHT := 1920
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_AAPT_CONFIG := normal
 
-PRODUCT_NAME := aosp_montana
+PRODUCT_NAME := viper_montana
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := montana
 PRODUCT_MODEL := Moto G5S
